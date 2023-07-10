de164ca0cf872bbf7c20b5b9ea333834acf57736
Yogesh Dangal <ydangal5@gmail.com>
2023-07-10 23:47:15 +0545

    removed skeleton-details

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -1,9 +1,8 @@
 import './details.css';
+import KeyValue from './KeyValue';
 import { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import SkeletonDetails from '../../components/skeleton/SkeletonDetails';
 import { ReactComponent as Arrow } from '../../assets/icons/arrow-left-long-solid.svg';
-import KeyValue from './KeyValue';
 
 const Details = ({ countries, toggle }) => {
   /**
@@ -60,6 +59,9 @@ const Details = ({ countries, toggle }) => {
       }));
     }
   }, [countries, urlCountry]);
+  useEffect(() => {
+    console.log(countryDetails);
+  }, [countryDetails])
   /**
    *  * Convert the country code to country name
    *  @param border is country code, like FRA for France
@@ -69,17 +71,11 @@ const Details = ({ countries, toggle }) => {
     return borderCountry ? borderCountry.name.common : '';
   };
   /**
-   * * Render Skeleton unitl details are not found
-   */
-  if (!countryDetails.country) {
-    return <SkeletonDetails toggle={toggle} />;
-  }
-  /**
    * * Render Missing section when user access details of undefined country
    */
   if (countryDetails.found) {
     return (
-      <section className={`missing flex flex-column ${toggle ? 'dark' : 'light'}`}>
+      <section className={`missing missing-details flex flex-column ${toggle ? 'dark' : 'light'}`}>
         <h1>No Details of country '<span>{urlCountry}</span>' was found.</h1>
         <p>
           Return to <Link to='/'>Homepage</Link>
@@ -91,30 +87,30 @@ const Details = ({ countries, toggle }) => {
   const { country, details } = countryDetails;
 
   return (
-    <section className={`details ${country.name.common.toLowerCase()}-details flex flex-column`}>
+    <section className={`details ${country?.name.common.toLowerCase()}-details flex flex-column`}>
       <Link to='/countries' className={`go-back flex ${toggle ? 'dark' : 'light'} br-5`}>
         <Arrow />
         <b>Back</b>
       </Link>
       <section className="details-section flex flex-column w-100">
         <section className={`flag-container ${toggle ? 'dark' : 'light'}`}>
-          {country.flags && (
-            <img src={country.flags.png} alt={country.flags.alt} title={`Flag of ${country.name.common}`} loading='lazy' />
+          {country?.flags && (
+            <img src={country.flags.png} alt={country.flags.alt} title={`Flag of ${country?.name.common}`} loading='lazy' />
           )}
         </section>
 
-        <section className={`country-info ${country.name && country.name.common.toLowerCase()}-info flex flex-column ${toggle ? 'dark' : 'light'}`}>
-          <h2 className='country-common-name'>{country.name && country.name.common}</h2>
+        <section className={`country-info ${country?.name && country.name.common.toLowerCase()}-info flex flex-column ${toggle ? 'dark' : 'light'}`}>
+          <h2 className='country-common-name'>{country?.name && country.name.common}</h2>
           <section className="country-stats w-100 flex flex-column">
             <section className={`country-stats-top flex flex-column ${toggle ? 'dark' : 'light'}`}>
               <KeyValue keyLabel='Native Name:' value={details.native} />
-              <KeyValue keyLabel='Population:' value={country.population?.toLocaleString()} />
-              <KeyValue keyLabel='Region:' value={country.region} />
-              <KeyValue keyLabel='Sub Region:' value={country.subregion.length === 0 ? 'No Subregion' : country.subregion} />
-              <KeyValue keyLabel='Capital:' value={country.capital?.[0] || 'No Capital'} />
+              <KeyValue keyLabel='Population:' value={country?.population.toLocaleString()} />
+              <KeyValue keyLabel='Region:' value={country?.region} />
+              <KeyValue keyLabel='Sub Region:' value={country?.subregion.length === 0 ? 'No Subregion' : country?.subregion} />
+              <KeyValue keyLabel='Capital:' value={country?.capital[0] || 'No Capital'} />
             </section>
             <section className={`country-stats-bottom flex flex-column ${toggle ? 'dark' : 'light'}`}>
-              <KeyValue keyLabel='Top-Level-Domain:' value={country.tld?.[0]} />
+              <KeyValue keyLabel='Top-Level-Domain:' value={country?.tld[0]} />
               <KeyValue keyLabel='Currencies:' value={details.currency} />
               <KeyValue keyLabel='Languages:' value={details.languages} />
             </section>
